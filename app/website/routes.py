3a9478adffebbf7da52b96af99eca92bac4146a6
Sunny <vermasunny012005@gmail.com>
2026-08-22 17:29:28 +0530

    connected Company media to about page

--- app/website/routes.py
+++ app/website/routes.py
@@ -1,13 +1,34 @@
 from flask import Blueprint, render_template
 
+
 # Create the website Blueprint
 website_bp = Blueprint(
     "website",
     __name__
 )
 
+
+# ==========================================
+# Homepage
+# ==========================================
+
 @website_bp.route("/")
 def home():
     """Homepage"""
 
-    return render_template("website/index.html")
+    return render_template(
+        "website/index.html"
+    )
+
+
+# ==========================================
+# About
+# ==========================================
+
+@website_bp.route("/about")
+def about():
+    """About BEST CONTROL"""
+
+    return render_template(
+        "website/about.html"
+    )
